2bdc5d24b035a95f16758fc5abbfbb6151f65618
kazu0m1 <65547812+kazu0m1@users.noreply.github.com>
2026-09-17 09:36:11 +0900

    Phase 6-2: move RecycleBinEntry model

--- Source/Ferry/RecycleBinService.cs
+++ Source/Ferry/RecycleBinService.cs
@@ -9,17 +9,6 @@ using System.Windows.Interop;
 
 namespace Ferry
 {
-    internal sealed class RecycleBinEntry
-    {
-        public string RecycledPath { get; set; }
-        public string MetadataPath { get; set; }
-        public string OriginalPath { get; set; }
-        public string Name { get; set; }
-        public bool IsDirectory { get; set; }
-        public long SizeBytes { get; set; }
-        public DateTime DeletedAt { get; set; }
-    }
-
     internal static class RecycleBinService
     {
         [DllImport("shell32.dll", CharSet = CharSet.Unicode)]
