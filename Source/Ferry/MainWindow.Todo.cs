3779cd6daf91b69e7fa35563a0acc09a87a63390
kazu0m1 <65547812+kazu0m1@users.noreply.github.com>
2026-09-26 10:21:47 +0900

    Restore fixed To-Do number column

--- Source/Ferry/MainWindow.Todo.cs
+++ Source/Ferry/MainWindow.Todo.cs
@@ -238,6 +238,7 @@ namespace Ferry
             TextBlock number = new TextBlock
             {
                 DataContext = entry,
+                Width = 28,
                 Margin = new Thickness(10, 7, 0, 5),
                 TextAlignment = TextAlignment.Left,
                 VerticalAlignment = VerticalAlignment.Top,
